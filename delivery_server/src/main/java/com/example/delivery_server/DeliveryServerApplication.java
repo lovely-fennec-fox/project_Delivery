cf3089f0a749c3a737686f1885f838a9daab5519
dr <love.pink.edacb1@gmail.com>
2019-07-30 19:04:06 +0900

    Set_springboot

---
+++ delivery_server/src/main/java/com/example/delivery_server/DeliveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.delivery_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryServerApplication.class, args);
+	}
+
+}
---
+++ delivery_server/src/test/java/com/example/delivery_server/DeliveryServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.delivery_server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DeliveryServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
